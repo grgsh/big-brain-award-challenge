cde4edf8baf9bb073ac4bef9eb2a99cb894172e9
Chapman Cheng <45029601+chapman-cc@users.noreply.github.com>
2025-05-28 14:37:11 +0100

    done

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-export default function (arr) {
-  //
-  // Your code goes here...
-  //
+/**
+ * @param {number[]} arr
+ * @returns {number[]}
+ */
+export default function answer(arr) {
+  /** @type {number[]} */
+  const zeros = [];
+  for (let i = 0; i < arr.length; i++) {
+    const number = arr[i];
+    if (number === 0) {
+      const [zero] = arr.splice(i, 1);
+      zeros.splice(zeros.length, 0, zero);
+      i -= 1;
+    }
+  }
 
+  arr.splice(arr.length, 0, ...zeros);
   return arr;
 }
