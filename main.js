75b222cc4bc3527673bd07debd9593076e516490
Peter Biro <github@vlx.dk>
2025-05-28 15:28:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 export default function (arr) {
-  //
-  // Your code goes here...
-  //
+  let writeIndex = 0;
+
+  // Move non-zero elements to the front
+  for (let readIndex = 0; readIndex < arr.length; readIndex++) {
+    if (arr[readIndex] !== 0) {
+      arr[writeIndex++] = arr[readIndex];
+    }
+  }
+
+  // Fill the rest with zeros
+  while (writeIndex < arr.length) {
+    arr[writeIndex++] = 0;
+  }
 
   return arr;
 }
